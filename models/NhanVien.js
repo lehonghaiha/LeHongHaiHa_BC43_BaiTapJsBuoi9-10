1562ce8dd5a684dc0fae1d6e48ce9471c4274057
unknown <halhhute@gmail.com>
2023-03-09 14:59:16 +0700

    Dan layout HTML

---
+++ models/NhanVien.js
@@ -0,0 +1,14 @@
+//Định nghĩa prototype nhân viên
+function NhanVien(){
+    this.taiKhoan = '';
+    this.hoTen = '';
+    this.email = '';
+    this.matKhau = '';
+    this.ngayLam = '';
+    this.luongCoBan = '';
+    this.chucVu = '';
+    this.gioLam = '';
+    this.tongLuong = '';
+    this.loaiNhanVien = '';
+    
+}   
